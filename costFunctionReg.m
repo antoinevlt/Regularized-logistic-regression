db1dd175fef65cf5c58fe4c6b4d42951316d6797
antoinevlt <viallet.antoine@gmail.com>
2016-10-06 11:54:19 +0200

    Add files via upload

--- costFunctionReg.m
+++ costFunctionReg.m
@@ -6,33 +6,17 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 
 % Initialize some useful values
 m = length(y); % number of training examples
-n=size(theta); 
 
-J = 0;
+
+J=0;
+J=-1/m*(y'*log(sigmoid(X*theta))+(1-y)'*log(1-sigmoid(X*theta)));
+J=J+lambda/(2*m)*(theta(2:end)'*theta(2:end));
+
 grad = zeros(size(theta));
-
-% Compute first term of cost function
-for i=1:m,
-J=J-y(i)*log(sigmoid(theta'*X(i,:)'))-(1-y(i))*log(1-sigmoid(theta'*X(i,:)'));
-end;
-% Compute second term of cost function
-for j=2:n, % first parameter is not regularized
-J=J+lambda/2*theta(j)^2;
-end;
-J=1/m*J;
-
-% Compute first coordinate of gradient of J(theta)
-for i=1:m,
-grad(1)=grad(1)+(sigmoid(theta'*X(i,:)')-y(i))*X(i,1);
-end;
-grad(1)=grad(1)/m;
-% Compute the rest of gradient of J(theta)
-for j=2:length(theta),
-  for i=1:m,
-  grad(j)=grad(j)+(sigmoid(theta'*X(i,:)')-y(i))*X(i,j);
-  end;
-grad(j)=(grad(j)+lambda*theta(j))/m;
-end;
+grad=1/m*X'*(sigmoid(X*theta)-y);
+term2=theta;
+term2(1)=0; % theta(1) is not regularized
+grad=grad+lambda/m*term2;
 
 
 % =============================================================
